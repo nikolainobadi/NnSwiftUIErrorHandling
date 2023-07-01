9b807cdc69a19ce25304b21ff31217512a9f8e5a
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-07-01 16:56:21 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NnSwiftUIErrorHandling
+
+A description of this package.
---
+++ Sources/NnSwiftUIErrorHandling/NnSwiftUIErrorHandling.swift
@@ -0,0 +1,6 @@
+public struct NnSwiftUIErrorHandling {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
